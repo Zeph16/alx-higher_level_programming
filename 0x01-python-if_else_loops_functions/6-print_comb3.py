05fe7bf8472da4b375dac24fd5940baf52a94e0a
Sofonias Ayele <sof16ayele@gmail.com>
2022-05-31 23:49:24 +0300

    Task 7

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for j in range(i + 1, 10):
-        print("{:d}{:d}, ".format(i, j), end='')
         if i == 8:
             print("89")
+            break
+        print("{:d}{:d}, ".format(i, j), end='')
