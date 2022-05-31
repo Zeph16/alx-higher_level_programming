690ab84c1a8adf32f85744bc76bd27dda39c51f7
Sofonias Ayele <sof16ayele@gmail.com>
2022-06-01 00:04:12 +0300

    Task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    number = number * -1 ? number < 0 : number
+    if number < 0:
+        number *= -1
     print(f"{number % 10}")
     return number % 10
