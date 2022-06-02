76a31bb1733796251a946178ae07b299e9b094f3
Sofonias Ayele <sof16ayele@gmail.com>
2022-06-02 22:19:54 +0300

    Task 1

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-from add_0 import add
+import add_0
+
+
 def main():
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))
+
+
 if __name__ == "__main__":
     main()
