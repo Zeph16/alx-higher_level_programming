486ffe6217cde6346946253e735de7317026a2fb
Sofonias Ayele <sof16ayele@gmail.com>
2022-06-06 20:09:53 +0300

    Different approach + tweaks

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,10 +4,6 @@
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list) or len(my_list) == 0:
         return "None"
-    templist = []
-    for i in range(len(my_list)):
-        if i == idx:
-            templist.append(element)
-            continue
-        templist.append(my_list[i])
+    templist = my_list.copy()
+    templist[idx] = element
     return templist
