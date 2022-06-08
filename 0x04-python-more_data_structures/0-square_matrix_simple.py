ac4eace6f48c5311427bc2fadb81c58ac273cc04
Sofonias Ayele <sof16ayele@gmail.com>
2022-06-08 20:57:00 +0300

    Task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -10,14 +10,3 @@ def square_matrix_simple(matrix=[]):
         square.append(row)
         row = []
     return square
-
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-new_matrix = square_matrix_simple(matrix)
-print(new_matrix)
-print(matrix)
