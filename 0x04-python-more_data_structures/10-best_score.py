464ea7c4d9f8d8bd673ef8a0b93ea56b973ece42
Sofonias Ayele <sof16ayele@gmail.com>
2022-06-08 22:49:11 +0300

    Task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,10 +2,10 @@
 
 
 def best_score(a_dictionary):
-    if a_dictionary == None or (a_dictionary.values()) == 0:
+    if a_dictionary is None or (a_dictionary.values()) == 0:
         return None
     key = ""
-    value = 0
+    value = -100
     for i in a_dictionary.keys():
         if a_dictionary[i] > value:
             value = a_dictionary[i]
