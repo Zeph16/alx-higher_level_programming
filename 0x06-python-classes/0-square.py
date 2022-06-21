950790567722bfbdd10998d9677ac7a629b4131e
Sofonias Ayele <sof16ayele@gmail.com>
2022-06-21 22:49:12 +0300

    Task 1 + a fix for 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-
-
 """
 Class Demo
 """
+
+
 class Square:
     """ Square Class """
     pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Class with private instance attribute demo
+"""
+
+
+class Square:
+    """ Square Class """
+
+    def __init__(self, size):
+        self.__size = size
