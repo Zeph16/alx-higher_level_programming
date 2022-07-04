21533b36c513dda774cf2a3d5067d3bd2767c3b1
Sofonias Ayele <sof16ayele@gmail.com>
2022-07-04 22:56:05 +0300

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Task 6 - Base Geometry
+"""
+
+
+class BaseGeometry:
+    """ Geometry Class """
+
+    def area(self):
+        """ Raises an Exception """
+        raise Exception("area() is not implemented")
