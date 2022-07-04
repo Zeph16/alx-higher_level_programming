62edb626ebaa8665bb3d831fb1d970e433576c12
Sofonias Ayele <sof16ayele@gmail.com>
2022-07-04 22:46:44 +0300

    Task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Task 4 - Is Kind Of Class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ Checks if obj is an instance of a_class or a subclass """
+    return isinstance(obj, a_class)
