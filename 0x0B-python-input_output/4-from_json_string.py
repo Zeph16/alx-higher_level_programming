eb791878117115293ca8a67a8cf5ab590ea7360f
Sofonias Ayele <sof16ayele@gmail.com>
2022-07-05 22:31:03 +0300

    Task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Task 4 - From JSON string to object
+"""
+import json
+
+
+def from_json_string(my_str):
+    """ Object from JSON String """
+    return json.loads(my_str)
