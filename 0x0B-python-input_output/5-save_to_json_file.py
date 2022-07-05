2a00706e8e4d7bc172fb1668a06073089ba56810
Sofonias Ayele <sof16ayele@gmail.com>
2022-07-05 22:38:54 +0300

    Task 6

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Task 5 - Saving an object to a file
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ Saves object json string to file """
+    with open(filename, 'w') as f:
+        f.write(json.dumps(my_obj)
