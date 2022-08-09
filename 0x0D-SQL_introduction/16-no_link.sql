df39262a80c52e14f76cba8c0e74184d57230769
Sofonias Ayele <sof16ayele@gmail.com>
2022-08-09 23:56:46 +0300

    Fix attempt for task 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- Shows all records in second_table without a name value sorted by score
-SELECT score, name FROM second_table where name IS NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE CHAR_LENGTH(name) > 0 ORDER BY score DESC;
