3f0ab21d4f5e37e1071bd6db6a724132be4def51
Sofonias Ayele <sof16ayele@gmail.com>
2022-08-09 23:48:31 +0300

    Fix attempt for task 7

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
--- Inserts a row first_table
-INSERT INTO first_table (id, name) VALUES (89, "Holberton School");
+-- Inserts a row in first_table
+INSERT INTO first_table ('id', 'name') VALUES ('89', 'Holberton School');
