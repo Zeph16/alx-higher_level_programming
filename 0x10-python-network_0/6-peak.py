cdd695b3f7ff28ebe7ac6637fe3e2bc1870f2b94
Sofonias Ayele <sof16ayele@gmail.com>
2022-09-23 00:02:25 +0300

    Fixed pycodestyle

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,15 +7,15 @@ def find_peak(list_of_integers):
     if list_of_integers == []:
         return None
 
-    l = len(list_of_integers)
-    m = int(l / 2)
+    len= len(list_of_integers)
+    m = int(len/ 2)
     li = list_of_integers
 
-    if m - 1 < 0 and m + 1 >= l:
+    if m - 1 < 0 and m + 1 >= len:
         return li[m]
     elif m - 1 < 0:
         return li[m] if li[m] > li[m + 1] else li[m + 1]
-    elif m + 1 >= l:
+    elif m + 1 >= len:
         return li[m] if li[m] > li[m - 1] else li[m - 1]
 
     if li[m - 1] < li[m] > li[m + 1]:
