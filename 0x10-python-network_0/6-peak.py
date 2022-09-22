595a380d395badc981abe39753b37aa71a1ee4ed
Sofonias Ayele <sof16ayele@gmail.com>
2022-09-23 00:05:59 +0300

    Task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,15 +7,15 @@ def find_peak(list_of_integers):
     if list_of_integers == []:
         return None
 
-    len= len(list_of_integers)
-    m = int(len/ 2)
+    length = len(list_of_integers)
+    m = int(length / 2)
     li = list_of_integers
 
-    if m - 1 < 0 and m + 1 >= len:
+    if m - 1 < 0 and m + 1 >= length:
         return li[m]
     elif m - 1 < 0:
         return li[m] if li[m] > li[m + 1] else li[m + 1]
-    elif m + 1 >= len:
+    elif m + 1 >= length:
         return li[m] if li[m] > li[m - 1] else li[m - 1]
 
     if li[m - 1] < li[m] > li[m + 1]:
